0c1f58c2e23ad4980e9313d60348b286e524cc56
AnsonT <tsaoa@acm.org>
2017-01-06 11:43:40 -0800

    docs(MenuExamplePointing): Sample code missing label
    Menu.Item content not defined.

--- docs/app/Examples/collections/Menu/Types/MenuExamplePointing.js
+++ docs/app/Examples/collections/Menu/Types/MenuExamplePointing.js
@@ -12,9 +12,9 @@ export default class MenuExamplePointing extends Component {
     return (
       <div>
         <Menu pointing>
-          <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-          <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick} />
-          <Menu.Item name='friends' active={activeItem === 'friends'} onClick={this.handleItemClick} />
+          <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} >Home</Menu.Item>
+          <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick} >Messages</Menu.Item>
+          <Menu.Item name='friends' active={activeItem === 'friends'} onClick={this.handleItemClick} >Friends</Menu.Item>
           <Menu.Menu position='right'>
             <Menu.Item>
               <Input icon='search' placeholder='Search...' />
